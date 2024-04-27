af03c8ebc7100405a1355878946136d97fdd0ea3
Feconicu <109251816+Feconicu@users.noreply.github.com>
2024-04-27 13:00:35 +0200

    Version used in the thesis

--- plot_generator.py
+++ plot_generator.py
@@ -1,6 +1,8 @@
 
 import matplotlib.pyplot as plt
-from math import sin, cos, radians, atan, pi
+from math import sin, cos, radians, atan, tan, pi
+from settings import FOLLOWER_MASS, INITIAL_FORCE, MINIMUM_DISPLACEMENT, SPRING_CONSTANT, ANGULAR_VELOCITY, FRICTION_COEFFICIENT, \
+    NUMBER_OF_SPRINGS
 
 class Cam:
     def __init__(self, factors, height, rf_angle, radious):
@@ -32,6 +34,15 @@ class Cam:
                 v += index/self.rf_angle*C*(angle/self.rf_angle)**(index-1) if index-1 > -1 else 0
         return v
 
+    def calculate_acceleration(self, angle):
+        angle %= 360
+        if angle >= self.rf_angle:
+            return 0
+        a = 0
+        for index, C in enumerate(self.factors):
+            a += (C*index*(index-1)*((angle/self.rf_angle)**(index-2)))/(self.rf_angle**2) if index > 1 else 0
+        return a
+
 
 class CamGrapher(Cam):
     def __init__(self, ang_speed, cam_or_factors, *args):
@@ -40,7 +51,9 @@ class CamGrapher(Cam):
             super().__init__(*cam_or_factors.__dir__())
         else:
             super().__init__(cam_or_factors ,*args)
-
+        print(self.factors)
+        self.max_acceleration = None
+  
     def graph_displacement(self, time_limit = 1, step = 0.01):
         time_axis = []
         displacement_axis = []
@@ -63,12 +76,27 @@ class CamGrapher(Cam):
             time_axis.append(t)
             velocity_axis.append(self.ang_speed*super().calculate_velocity(self.ang_speed*t))
             t += step
-        print(len(velocity_axis))
         plt.plot(time_axis, velocity_axis)
         plt.xlabel('t [s]')
         plt.ylabel('v [mm/s]')
         plt.title('v(t)')
         plt.show()
+
+    def graph_acceleration(self, time_limit = 1, step = 0.1, dont_show = False):
+        time_axis = []
+        acceleration_axis = []
+        t = 0
+        while t < time_limit:
+            time_axis.append(t)
+            acceleration_axis.append((self.ang_speed**2)*self.calculate_acceleration(self.ang_speed*t))
+            t +=step
+        self.max_acceleration = max(acceleration_axis, key=abs)
+        if not dont_show:
+            plt.plot(time_axis, acceleration_axis)
+            plt.xlabel('t [s]')
+            plt.ylabel('a [mm/s^2]')
+            plt.title('a(t)')
+            plt.show()
 
     def graph_shape(self, step = 0.1):
         x_cord = []
@@ -89,4 +117,56 @@ class CamGrapher(Cam):
         plt.xlim(-20,20)
         plt.ylim(-20,20)
         plt.axis('equal')
+        plt.show()
+
+    def calculate_spring_force(self, mass):
+        if self.max_acceleration is None:
+            self.graph_acceleration(dont_show=True, step = 0.001)
+        force = (abs(self.max_acceleration/1000) - 9.81)*1.5*mass
+        torque = (force + mass*9.81)*(self.height+self.radious)/1000
+        print('spring force: ', force)
+        print('max_torque: ', torque)
+
+    def graph_spring_force(self, safety_factor, mass, step=0.01):
+        gamma = 90
+        s_axis = []
+        force_axis = []
+        while gamma <= 180:
+            accel = ((-1)*self.ang_speed**2*self.calculate_acceleration(gamma)/1000 - 9.810)
+            accel = accel if accel > 0 else 0
+            s_axis.append(self.calculate_displacement(gamma))
+            force_axis.append(accel*safety_factor*mass)
+            gamma += step
+        plt.plot(force_axis, s_axis)
+        plt.ylabel('s [mm]')
+        plt.xlabel('P [N]')
+        plt.title('P(t)')
+        plt.show()
+
+    def get_shape_inventor_equation(self):
+        s_i = f'{self.radious}'
+        for i, factor in enumerate(self.factors):
+            s_i += f'+{abs(factor):f}*(t/{self.rf_angle})^{i}' if factor>0 else f'-{abs(factor):f}*(t/{self.rf_angle})^{i}'
+        print('s_i: ', s_i)
+
+    def graph_torque(self):
+        torque_axis = []
+        angle_axis = []
+        F_g = FOLLOWER_MASS*9.81
+        for angle in range(0, 180):
+            s_i = self.calculate_displacement(angle)
+            s_i_plus_r = s_i + self.radious     #mm
+
+            r_t = self.calculate_velocity(angle)    # mm/deg
+
+            F_s = (ANGULAR_VELOCITY**2)*self.calculate_acceleration(angle)/1000*FOLLOWER_MASS
+            F_p = (INITIAL_FORCE + (MINIMUM_DISPLACEMENT + s_i)*SPRING_CONSTANT)*NUMBER_OF_SPRINGS
+            angle_axis.append(angle)
+            torque_axis.append((F_g + F_p + F_s)*(r_t + FRICTION_COEFFICIENT*s_i_plus_r))   # mNm
+            print('F_p: ', F_p)
+
+        plt.plot(angle_axis, torque_axis)
+        plt.ylabel('M [Nmm]')
+        plt.xlabel('gamma [deg]')
+        plt.title('M(gamma)')
         plt.show()
--- settings.py
+++ settings.py
@@ -1,12 +1,32 @@
 import numpy as np
 
-HEIGHT = 6                                      #maximum displacement in millimeters
-RADIOUS = 14                                     #base circle radious in mm
-RF_PHASE_ANGLE = 180                             #sum of angles of rise and fall phases
+##############################################################################
+    # cam parameters #
+##############################################################################
+
+HEIGHT = 18                                     #maximum displacement in millimeters
+RADIOUS = 30                                    #base circle radious in mm
+RF_PHASE_ANGLE = 180                            #sum of angles of rise and fall phases
 BC = np.array([                                 #Boundry conditions Table - each row must be in [position (deg), lift (mm), velocity (mm/deg), acceleration (mm/deg^2)]
     [       0,      0,    0,    0],
     [RF_PHASE_ANGLE/2, HEIGHT, None, None],
     [  RF_PHASE_ANGLE,      0,    0,    0],
 ])
 
-ANGULAR_VELOCITY = 3600                          #in deg/s
+ANGULAR_VELOCITY = 1260                         #in deg/s
+FRICTION_COEFFICIENT = 0.6  #wcześniej liczono dla 0,8
+
+##############################################################################
+    # spring parameters #
+##############################################################################
+
+INITIAL_FORCE = 0.31                            # N
+SPRING_CONSTANT = 0.02                          # N/mm
+NUMBER_OF_SPRINGS = 4
+MINIMUM_DISPLACEMENT = 22.6                     # mm
+MAXIMUM_DISPLACEMENT = 40.6                     # mm
+##############################################################################
+    # platform parameters #
+##############################################################################
+
+FOLLOWER_MASS = 0.25                            # kg
